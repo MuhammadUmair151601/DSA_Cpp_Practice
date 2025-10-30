d0ca418773c49157282f085e75277952afe3c442
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-10-30 22:14:20 +0500

    files separated

---
+++ Loops.cpp
@@ -0,0 +1,59 @@
+
+// ********************************Loops*******************
+// while loop
+// practice printing numbers from 1 to 10:
+// #include <iostream>
+// using namespace std;
+// int main(){
+//       int n = 10;
+//       int i = 1 ;
+//       while (i<=10)
+//       {
+//         cout<<i<<endl;
+//         i++;
+//       }
+//       return 0;
+// }
+
+// Problem printing a line 100 times:
+// #include <iostream>
+// using namespace std;
+// int main(){
+//       int i=1;
+//       while (i<=100)
+//       {
+//         cout<<"i am a programmer"<<endl;
+//         i++;
+//       }
+//       return 0;
+// }
+
+// For Loop
+// sum of numbers from 1 to 10:
+// #include <iostream>
+// using namespace std;
+// int main(){
+//       int n = 10;
+//       int sum = 0;
+//       for(int i=1; i<=n; i++)
+//       {
+//         sum+=i;
+//       }
+//       cout<<"the sum of first 10 numbers is\n"<<sum<<endl;
+//       return 0;
+// }
+
+// applying in while loop:
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int i = 1;
+//   int sum = 0;
+//   while (i<=10)
+//   {
+//     sum+=i;
+//     i++;
+//   }
+//   cout<<"the sum of first 10 numbers is:"<<sum<<endl;
+//   return 0;
+// }
